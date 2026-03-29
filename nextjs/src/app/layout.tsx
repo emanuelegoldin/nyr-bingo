b93dfc8ffd9e6b9c73650d0c77619d1c5cb3c18b
emanuelegoldin <emanuele.goldin@gmail.com>
2026-03-29 14:07:57 +0000

    #999999 - Make request available by wrapping children in root layout

--- nextjs/src/app/layout.tsx
+++ nextjs/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
+import {NextIntlClientProvider} from 'next-intl';
 
 export const metadata: Metadata = {
   title: 'Resolution Bingo Team',
@@ -20,7 +21,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        {children}
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
         <Toaster />
       </body>
     </html>
